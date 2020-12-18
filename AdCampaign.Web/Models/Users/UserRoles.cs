10815fe61746b1a5d3cb6cc26ca2c817ebd23af6
Dias <standlightb@gmail.com>
2020-12-18 20:17:19 +0300

    Правка мелких багов

--- AdCampaign.Web/Models/Users/UserRoles.cs
+++ AdCampaign.Web/Models/Users/UserRoles.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AdCampaign.DAL.Entities;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -12,5 +13,20 @@ namespace AdCampaign.Models.Users
             new SelectListItem {Value = Role.Moderator.ToString("D"), Text = "Модератор"},
             new SelectListItem {Value = Role.Advertiser.ToString("D"), Text = "Рекламодатель"}
         };
+
+        public static string Localize(Role role)
+        {
+            switch (role)
+            {
+                case Role.Administrator:
+                    return "Администратор";
+                case Role.Moderator:
+                    return "Модератор";
+                case Role.Advertiser:
+                    return "Рекламодатель";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(role), role, null);
+            }
+        }
     }
 }
